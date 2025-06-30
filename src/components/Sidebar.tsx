a6a61438f6684313438636a3c770c2ea8939350e
JianuoZhu <jianuo.zhu@gmail.com>
2025-06-30 16:42:35 +0800

    new ui-sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,32 +1,35 @@
 
-import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems, SidebarLogo } from "flowbite-react";
+import { Sidebar, SidebarItem, SidebarItemGroup, SidebarItems, SidebarLogo} from "flowbite-react";
 import { HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
-
+import { NavLink } from "react-router-dom";
 export function Component() {
   return (
-    <Sidebar aria-label="Main Sidebar">
-      <SidebarLogo href="#" img="/favicon.svg" imgAlt="Wujie logo">
-        Manipulation Agent
+    <Sidebar aria-label="Main Sidebar" className="w-64 flex flex-col">
+      <SidebarLogo as={NavLink} href="/" to="/" img="/img/wujie_icon.svg" imgAlt="Wujie logo">
+        操作助手
       </SidebarLogo>
-      <SidebarItems>
-        <SidebarItemGroup>
-          <SidebarItem href="#" icon={HiChartPie}>
-            Dashboard
-          </SidebarItem>
-          <SidebarItem href="#" icon={HiViewBoards}>
-            视频监控
-          </SidebarItem>
-          <SidebarItem href="#" icon={HiInbox}>
-            远程控制
-          </SidebarItem>
-          <SidebarItem href="#" icon={HiUser}>
-            Agent任务
-          </SidebarItem>
-          <SidebarItem href="#" icon={HiShoppingBag}>
-            Logs
-          </SidebarItem>
-        </SidebarItemGroup>
-      </SidebarItems>
+      <hr className="border-b-1 border-gray-200 mx-4 w-[calc(100%-2rem)]" />
+      <div className="flex-1 flex items-center justify-center">
+        <SidebarItems>
+            <SidebarItemGroup>
+            <SidebarItem as={NavLink} to="/dashboard" icon={HiChartPie}>
+                Dashboard
+            </SidebarItem>
+            <SidebarItem as={NavLink} to="/video-control" icon={HiViewBoards}>
+                视频监控
+            </SidebarItem>
+            <SidebarItem as={NavLink} to="/remote-control" icon={HiInbox}>
+                远程控制
+            </SidebarItem>
+            <SidebarItem as={NavLink} to="/agent-tasks" icon={HiUser}>
+                Agent任务
+            </SidebarItem>
+            <SidebarItem as={NavLink} to="/logs" icon={HiShoppingBag}>
+                Logs
+            </SidebarItem>
+            </SidebarItemGroup>
+        </SidebarItems>
+      </div>
     </Sidebar>
   );
 }
